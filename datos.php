161d034173390d4cdc07bd6d06169d13bc88dc3a
Brandon Alexis Machado Ambuila <brandonmachado159@gmail.com>
2021-08-05 17:48:16 -0500

    consumo de datos de la API y redirecionarlos a la vista de la app

---
+++ datos.php
@@ -0,0 +1,42 @@
+<?php
+    session_start();
+
+    // ciudad recibida
+    $nombre_ciudad = $_POST['pais'];
+
+    //datos API REST para consumir
+    $llave_api = '4951e220d0e7efe17e9c9341421854db';
+    $direccion_api = 'https://api.openweathermap.org/data/2.5/weather?q='.$nombre_ciudad.'&appid='.$llave_api;
+
+    // Obtencion de datos en json
+    $datos_tiempo = json_decode(file_get_contents($direccion_api), true);
+
+    // Obtencion de datos API REST
+    $temperatura = $datos_tiempo['main']['temp'] - 273.15/* Grados C */;
+    $temperatura_maxima = $datos_tiempo['main']['temp_max'] - 273.15;
+    $temperatura_minima = $datos_tiempo['main']['temp_min'] - 273.15;
+    $tiempo_main = $datos_tiempo['weather'][0]['main'];
+    $humedad = $datos_tiempo['main']["humidity"];
+    $descripcion = $datos_tiempo['weather'][0]['description'];
+
+    /**
+     * Variables Session
+     */
+    $_SESSION['pais'] = $nombre_ciudad;
+    $_SESSION['temperatura'] = $temperatura;
+    $_SESSION['temperatura-max'] = $temperatura_maxima;
+    $_SESSION['temperatura-min'] = $temperatura_minima;
+    $_SESSION['tiempo-main'] = $tiempo_main;
+    $_SESSION['humedad'] = $humedad;
+    $_SESSION['description'] = $descripcion;
+    
+    // Redireccionamiento 
+    header('Location: index.php');
+?>
+
+
+
+
+
+
+
